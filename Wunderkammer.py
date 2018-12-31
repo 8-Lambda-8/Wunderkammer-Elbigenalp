21f5d0243e0e17b97e61e9bef3c8e249cb4611b3
8-Lambda-8 <j.wasle111@gmail.com>
2018-12-31 17:11:31 +0100

    1

--- Wunderkammer.py
+++ Wunderkammer.py
@@ -72,7 +72,8 @@ pygame.mouse.set_visible(False)
 
 vlcInstance = vlc.Instance()
 player = vlcInstance.media_player_new()
-player.toggle_fullscreen()
+player.set_hwnd(pygame.display.get_wm_info()['window'])
+#player.toggle_fullscreen()
 
 pygame.mixer.quit()
 
